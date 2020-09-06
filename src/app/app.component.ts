ea9aa06f8c4104ce5cef62c9b30f098d4d0774c9
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-09-05 20:40:09 -0500

    Add basic structure to read players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import Players from "./players";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'declarative-components-example';
+  title: string = "2020 Most paid players";
+  players: Player[] = [];
+
+  ngOnInit() {
+    this.players = this.getPlayers();
+  }
+
+  getPlayers = (): Player[] => {
+    return Players;
+  };
+}
+interface Player {
+  name: string;
+  age: number;
+  photo: string;
+  sport: string;
+  salary: number;
 }
---
+++ src/app/players.ts
@@ -0,0 +1,11 @@
+const Players = [
+  {
+    name: "Juan",
+    age: 30,
+    photo: "https://www.google.com/gatito",
+    sport: "soccer",
+    salary: 1000,
+  },
+];
+
+export default Players;
