a79d86645d49369ac83df3bc4350519cc9ce68ee
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-09-05 21:44:22 -0500

    Change sport to lower case

--- src/app/players.ts
+++ src/app/players.ts
@@ -4,7 +4,7 @@ const Players = [
     age: 39,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed53e8fa40c3d0007ed25b3%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D509%26cropX2%3D1693%26cropY1%3D78%26cropY2%3D1262",
-    sport: "Tennis",
+    sport: "tennis",
     salary: 106.3,
   },
   {
@@ -12,7 +12,7 @@ const Players = [
     age: 35,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec593cc431fb70007482137%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1321%26cropX2%3D3300%26cropY1%3D114%26cropY2%3D2093",
-    sport: "Soccer",
+    sport: "soccer",
     salary: 105,
   },
   {
@@ -20,7 +20,7 @@ const Players = [
     age: 33,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec595d45f39760007b05c07%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D989%26cropX2%3D2480%26cropY1%3D74%26cropY2%3D1564",
-    sport: "Soccer",
+    sport: "soccer",
     salary: 104,
   },
   {
@@ -28,7 +28,7 @@ const Players = [
     age: 28,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec59a8cda48890006292eee%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D845%26cropX2%3D2277%26cropY1%3D97%26cropY2%3D1528",
-    sport: "Soccer",
+    sport: "soccer",
     salary: 95.5,
   },
   {
@@ -36,7 +36,7 @@ const Players = [
     age: 35,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc5358798e4c00060d2274%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1184%26cropX2%3D3286%26cropY1%3D30%26cropY2%3D2130",
-    sport: "Basketball",
+    sport: "basketball",
     salary: 88.2,
   },
   {
@@ -44,7 +44,7 @@ const Players = [
     age: 32,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ece6eed1061240006af3da0%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1271%26cropX2%3D3410%26cropY1%3D153%26cropY2%3D2290",
-    sport: "Basketball",
+    sport: "basketball",
     salary: 74.4,
   },
   {
@@ -52,7 +52,7 @@ const Players = [
     age: 31,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ece6f50938ec500060aac25%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D3648%26cropY1%3D133%26cropY2%3D3784",
-    sport: "Basketball",
+    sport: "basketball",
     salary: 63.9,
   },
   {
@@ -60,7 +60,7 @@ const Players = [
     age: 44,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ece6ffa89ee2f0006814bfd%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D2074%26cropY1%3D14%26cropY2%3D2087",
-    sport: "Golf",
+    sport: "golf",
     salary: 62.3,
   },
   {
@@ -68,7 +68,7 @@ const Players = [
     age: 32,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc5f0a798e4c00060d233c%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D710%26cropX2%3D2526%26cropY1%3D28%26cropY2%3D1842",
-    sport: "Football",
+    sport: "football",
     salary: 60.5,
   },
   {
@@ -76,7 +76,7 @@ const Players = [
     age: 27,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc60a1fd6d6700060f84ff%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1058%26cropX2%3D2516%26cropY1%3D31%26cropY2%3D1488",
-    sport: "Football",
+    sport: "football",
     salary: 59.1,
   },
   {
@@ -84,7 +84,7 @@ const Players = [
     age: 32,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc644736d6f40008dcde32%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1349%26cropX2%3D3954%26cropY1%3D78%26cropY2%3D2685",
-    sport: "Boxing",
+    sport: "boxing",
     salary: 57,
   },
   {
@@ -92,7 +92,7 @@ const Players = [
     age: 31,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc655efd6d6700060f8511%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1155%26cropX2%3D4339%26cropY1%3D252%26cropY2%3D3434",
-    sport: "Basketball",
+    sport: "basketball",
     salary: 56,
   },
   {
@@ -100,7 +100,7 @@ const Players = [
     age: 35,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ece723454886200073da1eb%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D3003%26cropY1%3D199%26cropY2%3D3200",
-    sport: "Auto Racing",
+    sport: "auto racing",
     salary: 54,
   },
   {
@@ -108,7 +108,7 @@ const Players = [
     age: 31,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc696936d6f40008dcdee2%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D8%26cropX2%3D1931%26cropY1%3D139%26cropY2%3D2063",
-    sport: "Golf",
+    sport: "golf",
     salary: 52,
   },
   {
@@ -116,7 +116,7 @@ const Players = [
     age: 25,
     photo:
       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ecc696936d6f40008dcdee2%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D8%26cropX2%3D1931%26cropY1%3D139%26cropY2%3D2063",
-    sport: "Football",
+    sport: "football",
     salary: 49,
   },
 ];
