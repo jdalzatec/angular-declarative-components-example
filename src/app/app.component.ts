bb6d41b93256a020a3420a909893483a9073b1aa
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-09-05 22:15:39 -0500

    Adding declarative template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import Players from "./players";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title: string = "2020 Most paid players";
